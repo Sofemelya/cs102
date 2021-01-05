1df4a220767fda705e51bdfb42a9b35ec8e693d9
Sofemelya <oktyasof@gmail.com>
2021-01-05 20:19:33 +0300

    bb

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -66,7 +66,7 @@ def get_communities(net: tp.List[tp.Tuple[int, int]]) -> tp.Dict[int, tp.List[in
 
 def describe_communities(
     clusters: tp.Dict[int, tp.List[int]],
-    friends: tp.List[tp.Dict[str, tp.Any]],
+    friends: tp.List[tp.Dict[str, int]],
     fields: tp.Optional[tp.List[str]] = None,
 ) -> pd.DataFrame:
     if fields is None:
