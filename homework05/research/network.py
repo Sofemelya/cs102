b6873fe9d47318efe91f542ce167919aff3403d3
Sofemelya <70941023+Sofemelya@users.noreply.github.com>
2021-01-16 14:04:10 +0300

    Update network.py

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -5,7 +5,6 @@ import community as community_louvain
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
-
 from vkapi.friends import get_friends, get_mutual
 
 
@@ -17,7 +16,14 @@ def ego_network(
     :param user_id: Идентификатор пользователя, для которого строится граф друзей.
     :param friends: Идентификаторы друзей, между которыми устанавливаются связи.
     """
-    pass
+    network = []
+    if not friends:
+        get_friends(user_id).items  # type: ignore
+    mutual_friends = get_mutual(source_uid=user_id, target_uids=friends)
+    for target in mutual_friends:
+        for friend in target["common_friends"]:  # type: ignore
+            network.append((target["id"], friend))  # type: ignore
+    return network
 
 
 def plot_ego_network(net: tp.List[tp.Tuple[int, int]]) -> None:
