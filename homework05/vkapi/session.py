18a58c1706f089a1ae5e9155407ae83d82d147e6
Sofemelya <70941023+Sofemelya@users.noreply.github.com>
2021-01-29 17:39:33 +0300

    Update session.py

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -2,14 +2,12 @@ import typing as tp
 
 import requests
 from requests.adapters import HTTPAdapter
-from requests.exceptions import RetryError
 from requests.packages.urllib3.util.retry import Retry
 
 
-class Session(requests.Session):
+class Session:
     """
     Сессия.
-
     :param base_url: Базовый адрес, на который будут выполняться запросы.
     :param timeout: Максимальное время ожидания ответа от сервера.
     :param max_retries: Максимальное число повторных запросов.
@@ -17,24 +15,16 @@ class Session(requests.Session):
     """
 
     def __init__(
-        self, 
-        base_url: str, 
-        timeout: float = 5.0, 
-        max_retries: int = 3, 
+        self,
+        base_url: str,
+        timeout: float = 5.0,
+        max_retries: int = 3,
         backoff_factor: float = 0.3,
     ) -> None:
-        super().__init__()
-        self.retries = Retry(
-            total=max_retries, backoff_factor=backoff_factor, status_forcelist=[500]
-        )
-        self.mount(base_url, HTTPAdapter(max_retries=self.retries))
-        self.base_url = base_url
-        self.timeout = timeout
+        pass
 
-    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:  # type: ignore
-        kwargs["timeout"] = kwargs.get("timeout", self.timeout)
-        return super().get(f"{self.base_url}/{url}", *args, **kwargs)
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        pass
 
-    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:  # type: ignore
-        kwargs["timeout"] = kwargs.get("timeout", self.timeout)
-        return super().post(f"{self.base_url}/{url}", *args, **kwargs)
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        pass
