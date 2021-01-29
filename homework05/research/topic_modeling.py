18454634928f1dae25fc47e4f603be1a05958013
Sofemelya <70941023+Sofemelya@users.noreply.github.com>
2021-01-29 17:24:00 +0300

    Update topic_modeling.py

--- homework05/research/topic_modeling.py
+++ homework05/research/topic_modeling.py
@@ -9,16 +9,22 @@ from vkapi.wall import get_wall_execute
 def example():
     posts = get_wall_execute(domain="rbc", count=5000, max_count=1000, progress=tqdm)
     stopwords = list(map(str.strip, open("stop_words.txt")))
-    text_no_urls = map(preprocessing.replace.replace_urls, posts.text.dropna().to_list())
+    text_no_urls = map(
+        preprocessing.replace.replace_urls, posts.text.dropna().to_list()
+    )
     text_no_punct = map(preprocessing.remove_punctuation, text_no_urls)
     text_no_emojis = map(preprocessing.replace.replace_emojis, text_no_punct)
-    text_no_white_space = map(preprocessing.normalize.normalize_whitespace, text_no_emojis)
+    text_no_white_space = map(
+        preprocessing.normalize.normalize_whitespace, text_no_emojis
+    )
     docs = map(str.split, text_no_white_space)
     docs = [[word.lower() for word in doc if word not in stopwords] for doc in docs]
     docs = [[word for word in doc if word not in stopwords] for doc in docs]
     dictionary = Dictionary(docs)
     corpus = list(dictionary.doc2bow(text) for text in docs)
-    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word=dictionary, passes=15)
+    ldamodel = gensim.models.ldamodel.LdaModel(
+        corpus, num_topics=10, id2word=dictionary, passes=15
+    )
     vis = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
     pyLDAvis.show(vis)
 
